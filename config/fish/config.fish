b17356e26ede023dd37683c0cecc8aea3e9dc9a8
Adrian Schaedle <schaedles@gmail.com>
2012-11-10 22:57:52 -0500

    fuuuck

---
+++ config/fish/config.fish
@@ -0,0 +1,31 @@
+# Port of __git_ps1 for Fish shell (tested with FishFish)
+#--------------------------------------------------------
+# https://github.com/bjeanes/dot-files/tree/294254ce2f064bbfc586e98107da1ed510898ffd/fish/functions
+# via @bjeanes - https://github.com/bjeanes
+
+. ~/.config/fish/j.fish
+
+# Git
+alias g 'git status'
+alias gg 'git commit -am'
+
+alias f 'find . | grep'
+
+
+function fish_prompt --description 'Write out the prompt'
+  # printf '%s%s@%s%s ' (set_color green) (whoami) (hostname|cut -d . -f 1) (set_color normal) 
+ 
+  # Write the process working directory
+  if test -w "."
+    printf '%s%s' (set_color blue) (prompt_pwd)
+  else
+    printf '%s%s' (set_color cyan) (prompt_pwd)
+  end
+
+  printf ' %s%s ' (set_color yellow) (vcprompt)
+
+  # printf '%s$%s ' (set_color -o $fish_color_cwd) (set_color normal)
+ 
+  printf '%s\n→ ' (set_color normal)
+
+end
